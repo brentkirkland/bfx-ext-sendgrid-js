a86c3832cc583de698b826975bcbff2aa6c4e062
brentkirkland <brentckirkland@gmail.com>
2018-05-24 16:01:27 -0700

    test and working basic send

--- workers/api.sendgrid.ext.wrk.js
+++ workers/api.sendgrid.ext.wrk.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const { WrkApi } = require('bfx-wrk-api')
+const sgMail = require('@sendgrid/mail')
+const assert = require('assert')
 
 class WrkExtSendgridApi extends WrkApi {
   constructor (conf, ctx) {
     super(conf, ctx)
 
     this.loadConf('sendgrid.ext', 'ext')
+    this.checkConf()
 
     this.init()
     this.start()
@@ -14,6 +17,19 @@ class WrkExtSendgridApi extends WrkApi {
 
   init () {
     super.init()
+  }
+
+  checkConf () {
+    const { apiKey } = this.conf.ext
+
+    assert.equal(
+      typeof apiKey, 'string',
+      'apiKey must be a string, check config/sendgrid.ext.json'
+    )
+    assert(
+      apiKey.length > 0,
+      'set apiKey, check config/fx.ext.json'
+    )
   }
 
   getApiConf () {
@@ -24,9 +40,12 @@ class WrkExtSendgridApi extends WrkApi {
 
   getPluginCtx (type) {
     const ctx = super.getPluginCtx(type)
+    const { apiKey } = this.conf.ext
 
     switch (type) {
       case 'api_bfx':
+        ctx.sgMail = sgMail
+        ctx.sgMail.setApiKey(apiKey)
         break
     }
 
--- workers/loc.api/sendgrid.ext.js
+++ workers/loc.api/sendgrid.ext.js
@@ -1,12 +1,34 @@
 'use strict'
 
 const { Api } = require('bfx-wrk-api')
-const _ = require('lodash')
+// const _ = require('lodash')
 
 class ExtSendgrid extends Api {
   space (service, msg) {
     const space = super.space(service, msg)
     return space
+  }
+
+  async sendEmail (space, msg, cb) {
+    const { sgMail } = this.ctx
+    const {
+      to,
+      from,
+      subject,
+      text
+    } = msg
+
+    if (!to) return cb(new Error('ERR_API_NO_TO'))
+    if (!from) return cb(new Error('ERR_API_NO_FROM'))
+    if (!subject) return cb(new Error('ERR_API_NO_SUBJECT'))
+    if (!text) return cb(new Error('ERR_API_NO_TEXT'))
+
+    try {
+      const res = await sgMail.send(msg)
+      cb(null, res)
+    } catch (e) {
+      cb(new Error('ERR_API_SENDGRID'))
+    }
   }
 }
 
